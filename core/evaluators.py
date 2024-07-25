1) Solved the bug with .get method for memoization of the fidelity calculation results 2) Resolved the incorrect fidelity function implementation, where for some reason default prediction was used for samples, that are not covered by the rule

core/evaluators.py
@@ -110,7 +110,7 @@ class MemEvaluator(Evaluator):
 
         return diff
 
-    def binary_fidelity(self, rule, x, y, default=np.nan, ids=None):
+    def binary_fidelity(self, rule, x, y, ids=None):
         """Evaluate the goodness of rule.
         Args:
             rule (Unit): The unit to evaluate.
@@ -125,36 +125,36 @@ class MemEvaluator(Evaluator):
             y = self.oracle.predict(x).round().squeeze()
 
         if ids is None:
-            self.binary_fidelities[rule] = self.binary_fidelities.get(rule, binary_fidelity(rule, x, y, self,
-                                                                                            default=default, ids=None))
+            if rule not in self.binary_fidelities:
+                self.binary_fidelities[rule] = binary_fidelity(rule, x, y, evaluator=self)
             fidelity = self.binary_fidelities[rule]
         else:
-            fidelity = binary_fidelity(rule, x, y, self, default=default, ids=ids)
+            fidelity = binary_fidelity(rule, x, y, self, ids=ids)
 
         return fidelity
 
-    def binary_fidelity_model(self, units, x, y, k=1, default=None, ids=None):
-        """Evaluate the goodness of the `units`.
+    def binary_fidelity_model(self, rules, x, y, k=1, default=None, ids=None):
+        """Evaluate the goodness of the `rules`.
         Args:
-            units (Union(list, set)): The units to evaluate.
+            rules (Union(list, set)): The rules to evaluate.
             x (numpy.array): The data.
             y (numpy.array): The labels.
             k (int): Number of rules to use in the Laplacian prediction schema.
             default (int): Default prediction for records not covered by the unit.
             ids (numpy.array): Unique identifiers to tell each element in @c apart.
         Returns:
-              float: The units fidelity_weight.
+              float: The rules fidelity_weight.
         """
         if y is None:
             y = self.oracle.predict(x).squeeze().round()
 
-        fidelities = np.array([self.binary_fidelity(rule, x, y, default=default) for rule in units])
-        coverage = self.coverage(units, x)
+        fidelities = np.array([self.binary_fidelity(rule, x, y) for rule in rules])
+        coverage = self.coverage(rules, x)
 
-        if len(units) == 0:
+        if len(rules) == 0:
             predictions = [default] * y.shape[0]
         else:
-            rules_consequences = np.array([r.consequence for r in units])
+            rules_consequences = np.array([r.consequence for r in rules])
             # Fast computation for k = 1
             if k == 1:
                 weighted_coverage_scores = coverage * fidelities.reshape(-1, 1)  # Coverage matrix weighted by score

glocalx.py
@@ -203,8 +203,8 @@ class GLocalX:
 
         cut_rules = set()
         default = int(y.mean().round())
-        # Set ids to None to measure global fidelity
-        fidelities = np.array([self.evaluator.binary_fidelity(rule, x, y, default=default, ids=None)
+        # measure global fidelity
+        fidelities = np.array([self.evaluator.binary_fidelity(rule, x, y)
                                for rule in conflicting_group_list])
         dominant_rule = conflicting_group_list[np.argmax(fidelities).item(0)]
         cut_rules.add(dominant_rule)
@@ -248,7 +248,7 @@ class GLocalX:
 
         default = int(y.mean().round())
         # ids set to None to measure global fidelity_weight
-        fidelities = np.array([self.evaluator.binary_fidelity(r, x, y, default=default, ids=None) for r in rules_list])
+        fidelities = np.array([self.evaluator.binary_fidelity(r, x, y) for r in rules_list])
         best_rule = rules_list[np.argmax(fidelities).item(0)]
 
         # Features shared by all
@@ -518,8 +518,8 @@ class GLocalX:
         rules_1 = [r for r in fine_boundary if r.consequence == 1]
 
         if strategy == 'fidelity':
-            values_0 = [evaluator.binary_fidelity(rule, x, y, default=default_y) for rule in rules_0]
-            values_1 = [evaluator.binary_fidelity(rule, x, y, default=default_y) for rule in rules_1]
+            values_0 = [evaluator.binary_fidelity(rule, x, y) for rule in rules_0]
+            values_1 = [evaluator.binary_fidelity(rule, x, y) for rule in rules_1]
         elif strategy == 'coverage':
             values_0 = [evaluator.coverage(rule, x) for rule in rules_0]
             values_1 = [evaluator.coverage(rule, x) for rule in rules_1]

glocax_probabilistic.py
@@ -29,7 +29,7 @@ class ProbabilisticCutGLocalX(GLocalX):
         cut_rules = set()
         default = int(y.mean().round())
         # Set ids to None to measure global fidelity_weight
-        fidelities = np.array([self.evaluator.binary_fidelity(rule, x, y, default=default, ids=None)
+        fidelities = np.array([self.evaluator.binary_fidelity(rule, x, y)
                                for rule in conflicting_group_list])
         # maximum fidelities across fidelities array *except* at position i
         masked_max_fidelities = np.array([np.delete(fidelities, i).max() for i in range(n)])

utilities/coverage_utilities.py
@@ -15,9 +15,11 @@ def covers(rule: Rule, x: np.array):
     """
     return all([(x[feature] >= lower) & (x[feature] < upper)] for feature, (lower, upper) in rule)
 
-
-def binary_fidelity(rule: Unit, x, y, evaluator=None, ids=None, default=np.nan):
-    """Evaluate the goodness of rule: 1-hamming distance -> the higher the better
+def binary_fidelity_old(rule: Unit, x, y, evaluator=None, ids=None, default=np.nan):
+    """
+    Problem 1: this method for some reason takes into acount the default value for the prediction, which only makes it worse for rule's assessment
+    Problem 2: this method does not take into account how many samples the rule covers (obviously the more the better)
+    Evaluate the goodness of rule 1-hamming distance -> the higher the better
     Args:
         rule (Unit): The rule to evaluate.
         x (numpy.array): The data.
@@ -39,6 +41,36 @@ def binary_fidelity(rule: Unit, x, y, evaluator=None, ids=None, default=np.nan):
     y = y.squeeze()
 
     fidelity = 1 - hamming(unit_predictions, y[ids] if ids is not None else y) if len(y) > 0 else 0
+
+    return fidelity
+
+def binary_fidelity(rule: Unit, x, y, evaluator=None, ids=None):
+    """Evaluate the goodness of rule via "1-hamming distance" -> the higher the better
+        Note: Compaired to the old version, here we do not take into account default prediction for all the other samples, which are not covered by the rule
+    Args:
+        rule (Unit): The rule to evaluate.
+        x (numpy.array): The data.
+        y (numpy.array): The labels.
+        evaluator (Evaluator): Optional evaluator to speed-up computation.
+        ids (numpy.array): Unique identifiers to tell each element in @patterns apart.
+    Returns:
+          float: The rule's fidelity_weight
+    """
+    # Calculate coverage of the rule (ids that are under rule's scope)
+    coverage = evaluator.coverage(rule, x, ids=ids).flatten()
+    covered_indices = np.where(coverage)[0]
+    if len(covered_indices) == 0:
+        return 0
+    
+    # Predictions for covered samples (just zeroes or ones)
+    unit_predictions = np.full(len(covered_indices), rule.consequence)
+    
+    y = y.squeeze()
+    covered_y = y[covered_indices]
+
+    # Calculate the fidelity as 1 - Hamming distance for covered samples
+    hamming_distance = hamming(unit_predictions, covered_y)
+    fidelity = 1 - hamming_distance
 
     return fidelity
 